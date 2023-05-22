e3756c1788165f1fbd1ae3a4a8671c6284eed613
Johan Karlsson <johan.o.karlsson@slu.se>
2023-05-22 21:59:39 +0200

    Added FertiliserMgmt to __init__

--- CIBUSmod/__init__.py
+++ CIBUSmod/__init__.py
@@ -5,5 +5,6 @@ from .production_systems.crop_prod import CropProduction
 from .production_systems.animal_herd import CattleHerd, PigHerd
 from .production_systems.manure_mgmt import ManureMgmt
 from .production_systems.feed_mgmt import FeedMgmt
+from .production_systems.fertiliser_mgmt import FertiliserMgmt
 
 from .optimisation.geo_dist import GeoDistributor
