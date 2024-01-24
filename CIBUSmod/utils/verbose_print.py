7a26e0e8b2c7f696e60581bae88e2ef72a87a800
Johan Karlsson <johan.o.karlsson@slu.se>
2024-01-24 21:18:37 +0100

    Print timing for all steps

--- CIBUSmod/utils/verbose_print.py
+++ CIBUSmod/utils/verbose_print.py
@@ -1,26 +1,32 @@
 import time
 
 # Function for printing progress messages with time-stamps if verbose==True
-def verbose_init(verbose, id_str='', head_len=62):
+def verbose_init(verbose, id_str=''):
     if verbose:
         tic = time.time()
+        sub_tic = [None]
         if id_str != '':
             id_str = f'[{id_str}]'
-        def verbose_print(*args, type='prog', sep=' ',end='\n'):
-                        if type=='head':
-                            heading = sep.join(args)
-                            stars = '*' * int((head_len - len(heading)) / 2 - 2)
-                            return print('\n',stars,heading,stars,'\n', sep=sep, end=end)
-                        elif type=='end':
-                            time_stamp = f'[{time.strftime("%H:%M:%S",time.localtime())}]'
-                            return print(f'{time_stamp}{id_str}',*args, f'Done! Elapsed time: {(time.time()-tic):.0f} sec',sep=sep,end=end)
-                        else:
-                            time_stamp = f'[{time.strftime("%H:%M:%S",time.localtime())}]'
-                            return print(f'{time_stamp}{id_str}',*args, sep=sep, end=end)
+        def verbose_print(*args, type='prog'):
+                        
+            if not sub_tic[0] is None:
+                sub_timing = f' {(time.time()-sub_tic[0]):.1f}s\n'
+                sub_tic[0] = time.time()
+            else:
+                    sub_timing = ''
+                    sub_tic[0] = time.time()
+
+            if type=='end':
+                time_stamp = f'[{time.strftime("%H:%M:%S",time.localtime())}]'
+                return print(f'{sub_timing}{time_stamp}{id_str}',*args, f'Done! Elapsed time: {(time.time()-tic):.0f} sec',sep=' ',end='\n')
+            else:
+                
+                time_stamp = f'[{time.strftime("%H:%M:%S",time.localtime())}]'
+                return print(f'{sub_timing}{time_stamp}{id_str}',*args, sep=' ', end='')
                         
     else:
         # empty function for not printing
         def verbose_print(*args,**kwargs):
-            pass
+            return None
     
     return verbose_print
