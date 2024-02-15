8e37b27b87183bfcf1070ac501ac163281ea42c2
Johan Karlsson <johan.o.karlsson@slu.se>
2024-02-15 09:25:55 +0100

    Fixed problem with module/attribute matching due to capitalization

--- CIBUSmod/utils/session_db.py
+++ CIBUSmod/utils/session_db.py
@@ -979,7 +979,9 @@ f'''{module}
                 msg = f"No match for module='{module}' and attr='{attr}'"
                 raise ValueError(msg)
             elif len(all_res) > 1:
-                exact = [r for r in all_res if (r[2] == attr and module in r[1]) or (r[2] in attr and module == r[1])]
+                exact = [r for r in all_res
+                         if (r[2].capitalize() == attr.capitalize() and module.capitalize() in r[1].capitalize())
+                         or (attr.capitalize() in r[2].capitalize() and r[1].capitalize() == module.capitalize())]
                 if len(exact) == 1:
                     attr_id = exact[0][0]
                     module = exact[0][1]
