733b8bd86d8e24d3968e8bf3e5b03c31c207ea4d
Johan Karlsson <johan.o.karlsson@slu.se>
2024-03-08 19:29:59 +0100

    Fixed sub_timing if 'msg' type used

--- CIBUSmod/utils/verbose_print.py
+++ CIBUSmod/utils/verbose_print.py
@@ -8,18 +8,19 @@ def verbose_init(verbose, id_str=''):
         if id_str != '':
             id_str = f'[{id_str}]'
         def verbose_print(*args, type='prog'):
-                        
-            if not sub_tic[0] is None:
-                sub_timing = f'{(time.time()-sub_tic[0]):.1f}s\n'
-                sub_tic[0] = time.time()
-            else:
+            
+            if type != 'msg':
+                if not sub_tic[0] is None:
+                    sub_timing = f'{(time.time()-sub_tic[0]):.1f}s\n'
+                    sub_tic[0] = time.time()
+                else:
                     sub_timing = ''
                     sub_tic[0] = time.time()
 
-            if type=='end':
+            if type == 'end':
                 time_stamp = f'[{time.strftime("%H:%M:%S",time.localtime())}]'
                 return print(f'{sub_timing}{time_stamp}{id_str}',*args, f'Done! Elapsed time: {(time.time()-tic):.0f} sec', sep=' ', end='\n')
-            elif type=='msg':
+            elif type == 'msg':
                 return print(*args, sep=' ', end=' ')
             else:
                 time_stamp = f'[{time.strftime("%H:%M:%S",time.localtime())}]'
