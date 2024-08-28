f4fac15a9163fea994618e1003a0d14e74a22191
Johan Karlsson <47713385+karlssonjo@users.noreply.github.com>
2024-08-28 13:55:40 +0200

    Added emissions rated to WasteAndCircularity module to 'get_emissions' function

--- CIBUSmod/utils/output_data_manip_db.py
+++ CIBUSmod/utils/output_data_manip_db.py
@@ -31,11 +31,14 @@ def get_emissions(session, scn='all', years='all', interpolate=False):
         'manure management' : {
             'module' : ['AnimalHerd'],
             'attr' : ['manure.N_loss',
+                      'manure.P_loss',
+                      'manure.K_loss',
                       'manure.VS_loss']
         },
         'energy use' : {
             'module' : ['AnimalHerd',
-                        'CropProduction'],
+                        'CropProduction',
+                        'WasteAndCircularity'],
             'attr' : ['energy_use_emissions',
                       'energy_use_supply_chain_emissions']
         },
@@ -49,11 +52,20 @@ def get_emissions(session, scn='all', years='all', interpolate=False):
             'module' : ['CropProduction'],
             'attr' : ['fertiliser.manure_N_application_loss',
                       'fertiliser.manure_N_soil_loss',
+                      'fertiliser.organic_N_application_loss',
+                      'fertiliser.organic_N_soil_loss',
                       'fertiliser.mineral_N_application_loss',
                       'fertiliser.mineral_N_soil_loss',
                       'fertiliser.crop_residues_N_soil_loss',
                       'fertiliser.organic_soil_N_loss',
                       'fertiliser.leaching_N']
+        },
+        'waste and circularity' : {
+            'module' : ['WasteAndCircularity'],
+            'attr' : ['losses_N',
+                      'losses_P',
+                      'losses_K',
+                      'losses_VS']
         }
     }
 
@@ -75,6 +87,19 @@ def get_emissions(session, scn='all', years='all', interpolate=False):
                         interpolate = interpolate
                     )
                     df = df.rename_axis(columns = {'crop_group2' : 'item'})
+                elif md == 'WasteAndCircularity':
+                    df = session.get_attr(
+                        module = 'WasteAndCircularity',
+                        attr = at,
+                        groupby = {'treatment':None,
+                                   'region':None, 'compound':None},
+                        scn = scn,
+                        years = years,
+                        interpolate = interpolate
+                    )
+                    df = df.rename_axis(columns = {'treatment' : 'item'})
+                    # Add 'prod_system' to column level as not applicable (n/a)
+                    df = pd.concat({'n/a': df}, names=['prod_system'], axis=1)
                 elif md == 'AnimalHerd':
                     df = session.get_attr(
                         module = 'AnimalHerd',
