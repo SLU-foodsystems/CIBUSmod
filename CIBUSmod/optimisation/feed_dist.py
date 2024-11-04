384330e5345ae23cf97d5278bf91e5878165a314
Wilhelm Wanecek <wilhelm@wanecek.com>
2024-11-04 14:50:35 +0100

    Add C13: constrain total import

--- CIBUSmod/optimisation/feed_dist.py
+++ CIBUSmod/optimisation/feed_dist.py
@@ -2259,6 +2259,100 @@ class FeedDistributor:
 
         return IndexedMatrix.from_coordinates((val, (row_nr, col_nr)), row_idx, col_idx)
 
+    def make_B13(self, prod_type: Literal["crop_prod", "by_prod"]) -> pd.Series:
+        """ """
+        self.feed_mgmt.par.clear()
+
+        par = "max_total_imported"
+
+        pss = self.x_idx["crp"].get_level_values("prod_system").unique()
+        cps = self.feed_mgmt.par.get_unique(prod_type, qry=f'parameter=="{par}"')
+
+        data = self.feed_mgmt.par.get_from_frame(
+            par,
+            pd.DataFrame(
+                columns=pd.Index(pss, name="prod_system"),
+                index=pd.Index(cps, name=prod_type),
+            ),
+        ).unstack()
+
+        assert isinstance(data, pd.Series)
+        return data
+
+    def make_A13(
+        self, prod_type: Literal["crop_prod", "by_prod"], row_idx: pd.Index
+    ) -> IndexedMatrix:
+        self.feed_mgmt.par.clear()
+
+        # Get col index from crop production (cr,ps,re)
+        col_idx = self.x_idx["fds"]
+
+        # To store data and corresponding row/col numbers for constructing matrix
+        val = []
+        row_nr = []
+        col_nr = []
+
+        for row_i, (ps, cp_or_byp) in enumerate(row_idx):
+            feed_to_prod = self.get_feed_to_crop_prod_factors(prod_type, prod_system=ps)
+            for col_i, col in enumerate(col_idx):
+                feed = col[0]
+                feed_ps = col[3]
+                if feed_ps != ps:  # TODO: Verify
+                    continue
+
+                if (feed, cp_or_byp) not in feed_to_prod.index:
+                    continue
+
+                val.append(
+                    feed_to_prod.loc[(feed, cp_or_byp), "feed_to_prod"]
+                    * feed_to_prod.loc[(feed, cp_or_byp), "share_imported"]
+                )
+                row_nr.append(row_i)
+                col_nr.append(col_i)
+
+        # Create Compressed Sparse Column matrix
+        return IndexedMatrix.from_coordinates(
+            (val, (row_nr, col_nr)),
+            row_idx,
+            col_idx,
+        )
+
+    def make_C13(self):
+        """
+        Ensure that the imports of feed do not exceed the max_total_imported parameter.
+
+        The A-matrix maps feeds to their import-shares, so that we get the volume we
+        import, while the B matrix contains the ceiling of max imported volume. This is
+        done per prod_sys and feed product, for both main- and by-products.
+        """
+        B13_cp = self.make_B13("crop_prod")
+        B13_by = self.make_B13("by_prod")
+        B13 = np.concatenate([np.array(B13_cp.values), np.array(B13_by.values)])
+
+        A13_cp = self.make_A13("crop_prod", B13_cp.index)
+        A13_by = self.make_A13("by_prod", B13_by.index)
+
+        n_rows = A13_cp.shape[0] + A13_by.shape[0]
+        A13 = scipy.sparse.hstack(
+            [
+                scipy.sparse.csc_matrix((n_rows, len(self.x_idx["ani"]))),
+                scipy.sparse.csc_matrix((n_rows, len(self.x_idx["crp"]))),
+                scipy.sparse.vstack([A13_cp.M, A13_by.M], format="csc"),
+            ],
+            format="csc",
+        )
+
+        self.constraints.update(
+            {
+                "C14: A14 @ x >= 0": {
+                    "left": lambda x, A13, B13: A13 @ x - B13,
+                    "right": lambda **kwargs: 0,
+                    "rel": "<=",
+                    "pars": {"A13": A13, "B13": B13},
+                }
+            }
+        )
+
     def make_P1_1(self):
         """
         Creates the P_{1,1} matrix, which is an identity matrix of size len(x0['ani'])
