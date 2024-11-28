0f14e3a15ae9db1b6ae2665cebec6c2833d0bf18
Wilhelm Wanecek <wilhelm@wanecek.com>
2024-11-28 15:08:46 +0100

    Rephrase C11-equality constr. as min/max

--- CIBUSmod/optimisation/feed_dist.py
+++ CIBUSmod/optimisation/feed_dist.py
@@ -1276,9 +1276,32 @@ class FeedDistributor:
 
         # Create A-matrices for each of the parameters. If there is no data for any
         # given parameter, we will not add that constraint.
-        A11_eq = with_zeroes(self.make_A11("share_in_ration"))
-        A11_min = with_zeroes(self.make_A11("min_share_in_ration"))
-        A11_max = with_zeroes(self.make_A11("max_share_in_ration"))
+        A11_eq = self.make_A11("share_in_ration")
+
+        A11_min = self.make_A11("min_share_in_ration")
+        A11_max = self.make_A11("max_share_in_ration")
+
+        def combine_A11s(
+            A11_eq: IndexedMatrix, A11_minmax: None | IndexedMatrix, mul_factor: float
+        ):
+            if A11_minmax is None:
+                A11_min = A11_eq.copy()
+                A11_min.M = A11_min.M.multiply(mul_factor)
+                return A11_min
+
+            return IndexedMatrix(
+                scipy.sparse.csc_matrix.maximum(
+                    A11_minmax.M,
+                    A11_eq.M.multiply(mul_factor),
+                ),
+                A11_minmax.rows,
+                A11_minmax.cols,
+            )
+
+        tol = 0.01
+        if A11_eq is not None:
+            A11_min = with_zeroes(combine_A11s(A11_eq, A11_min, 1 - tol))
+            A11_max = with_zeroes(combine_A11s(A11_eq, A11_max, 1 + tol))
 
         C11s: dict[str, Constraint] = {}
 
@@ -1288,14 +1311,6 @@ class FeedDistributor:
                 "right": lambda A11: 0,
                 "rel": ">=",
                 "pars": {"A11": A11_min},
-            }
-
-        if A11_eq is not None:
-            C11s["C11 (eq): A11 @ x == 0"] = {
-                "left": lambda x, A11: A11.M @ x,
-                "right": lambda A11: 0,
-                "rel": "==",
-                "pars": {"A11": A11_eq},
             }
 
         if A11_max is not None:
