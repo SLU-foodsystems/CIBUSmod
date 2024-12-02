6f839378c6e579522eda4c68bc39de177cd75828
Johan Karlsson <johan.o.karlsson@slu.se>
2024-12-02 12:26:10 +0100

    Docstring and pass on scn, years to get_attr

--- CIBUSmod/impact/biodiv.py
+++ CIBUSmod/impact/biodiv.py
@@ -1,7 +1,27 @@
 import numpy as np
 
 def get_crop_div(session, groupby='all', scn='all', years = 'all', method='Shannon', crop_group='crop_group', land_use='cropland', interpolate=False):
-    '''
+    '''Gives crop diversity index
+
+    Parameters
+    ----------
+    session : Session object
+    groupby : str, list or dict, default 'all'
+        If str or list data is grouped and aggregated by these levels.
+        If 'all' all available levels are returned
+        If 'none'  data is summed over all levels
+        If a dict is supplied relation tables are used
+    scn : (list of) str
+    years : (list of) str
+    method : str, default 'factors'
+        Method to calculate crop diversity
+            'Shannon' = Shannon Diversity Index
+    crop_group : str, default 'crop_group'
+        Aggregation level to use in calculating diversity
+    land_use : str, default 'cropland'
+        Land use to calculate crop diversity for
+    interpolate : Bool, default False
+            If True interpolate between defined years
     '''
 
     if groupby == 'all':
@@ -38,7 +58,14 @@ def get_crop_div(session, groupby='all', scn='all', years = 'all', method='Shann
 
 
     res = (
-        session.get_attr('CropProduction','area', groupby_get)
+        session.get_attr(
+            module = 'CropProduction',
+            attr = 'area',
+            groupby = groupby_get,
+            scn = scn,
+            years = years,
+            interpolate = interpolate
+        )
         .loc[:,land_use]
         # Calculate proportion of cropland use per crop_group in for each group
         .T.groupby(groupby_after_get).transform(lambda x: x/x.sum()).T
