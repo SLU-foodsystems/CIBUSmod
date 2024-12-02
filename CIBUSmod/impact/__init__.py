85bb96d76d1938bcb758eb400fae966ab6a331be
Johan Karlsson <johan.o.karlsson@slu.se>
2024-12-02 12:22:24 +0100

    Added function for crop diversity

--- CIBUSmod/impact/__init__.py
+++ CIBUSmod/impact/__init__.py
@@ -4,4 +4,5 @@ IMPACT_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data
 
 from .general import get_emissions, to_ICBM
 from .climate import get_GHG
+from .biodiv import get_crop_div
 from .livestock import get_LSU
