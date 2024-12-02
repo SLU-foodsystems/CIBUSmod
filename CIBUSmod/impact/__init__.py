48ce6623cefd6a8c4b347cde219af490718f9e85
Johan Karlsson <johan.o.karlsson@slu.se>
2024-12-02 11:25:56 +0100

    Added function for LSU

--- CIBUSmod/impact/__init__.py
+++ CIBUSmod/impact/__init__.py
@@ -4,3 +4,4 @@ IMPACT_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data
 
 from .general import get_emissions, to_ICBM
 from .climate import get_GHG
+from .livestock import get_LSU
