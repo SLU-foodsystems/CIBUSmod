003e6d231604da5b188215de119e5b5ee03e1e65
Johan Karlsson <johan.o.karlsson@slu.se>
2024-12-09 15:30:39 +0100

    Added function for time-dynamic climate impact

--- CIBUSmod/impact/__init__.py
+++ CIBUSmod/impact/__init__.py
@@ -3,6 +3,6 @@ import os
 IMPACT_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data')
 
 from .general import get_emissions, to_ICBM
-from .climate import get_GHG
+from .climate import get_GHG, get_deltaT
 from .biodiv import get_crop_div
 from .livestock import get_LSU
--- CIBUSmod/impact/climate.py
+++ CIBUSmod/impact/climate.py
@@ -1,8 +1,11 @@
 import os
 import pandas as pd
+import numpy as np
 
 from . import  IMPACT_DATA_PATH
 from .general import get_emissions
+from ..temp_calc.temp_utils import ghg_to_temp
+from ..utils.session_db import Session
 
 def get_GHG(session, scn='all', years = 'all', CO2eq='GWP100 AR4', interpolate=False):
     '''
@@ -49,3 +52,80 @@ def get_GHG(session, scn='all', years = 'all', CO2eq='GWP100 AR4', interpolate=F
         )
 
     return res
+
+def get_deltaT(
+    session : Session,
+    groupby : list[str]|str = 'all',
+    scn : str = 'all',
+    years : str = 'all',
+    extend : int = 0
+):
+
+    # Get greenhouse gas emissions
+    print('Getting GHG emissions ...')
+    ghg = get_GHG(session, scn, years, CO2eq=None, interpolate=True)
+
+    if groupby == 'all':
+        groupby = ['process', 'sub-process', 'prod_system', 'item', 'region', 'compound']
+    elif groupby == 'none':
+        groupby = []
+    elif isinstance(groupby, str):
+        groupby = [groupby]
+
+    groupby_orig = groupby.copy()
+
+    # Make sure 'compound' is present and last in list
+    if 'compound' in groupby:
+        groupby.remove('compound')
+    groupby += ['compound']
+
+    rename_ghg = {
+        'CO2' : 'co2',
+        'CH4bio' : 'ch4',
+        'CH4fos' : 'ch4',
+        'N2O' : 'n2o',
+        'N2Oind' : 'n2o'
+    }
+
+    # Group and sum
+    ghg = ghg.T.groupby(groupby).sum().T
+
+    start_year = ghg.index.get_level_values('year').astype(int).min()
+    end_year = ghg.index.get_level_values('year').astype(int).max()
+
+    # Costruct output dataframe with extended year index
+    scns = ghg.index.unique('scn')
+    deltaT = pd.DataFrame(
+        0.0,
+        index=pd.MultiIndex.from_product(
+            [scns, map(str, range(start_year, end_year + extend + 1))],
+            names=['scn', 'year']
+        ),
+        columns=ghg.columns
+    )
+
+    print('Calculating temperature response ...')
+    for scn in deltaT.index.unique('scn'):
+        for col in deltaT.loc[scn].columns:
+            # Get GHG time-series for col
+            ghg_time_series = ghg.loc[scn,col]
+            # Pre-compute temp response curves
+            cmp = col[-1] if isinstance(col, tuple) else col
+            temp_curve = ghg_to_temp(ghg = rename_ghg[cmp], time_horizon=end_year+extend-start_year)
+            # Calculate temperature response
+            temp_resp = sum([
+                np.concatenate([
+                    np.zeros(y-start_year),
+                    temp_curve[0:end_year+extend-y+1] * ghg_time_series.loc[str(y)]
+                ]) for y in range(start_year, end_year + 1)
+            ])
+            # Store results
+            deltaT.loc[scn,col] = temp_resp
+
+    if groupby != groupby_orig:
+        if len(groupby_orig)>0:
+            deltaT = deltaT.T.groupby(groupby_orig).sum().T
+        else:
+            deltaT = deltaT.sum(axis=1)
+
+    return deltaT
