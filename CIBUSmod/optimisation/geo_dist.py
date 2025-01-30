57f35578e87e83f6b48160c7f030098f15a9bfab
Wilhelm Wanecek <wilhelm@wanecek.com>
2025-01-30 15:33:09 +0100

    Exclude certain imports unless TYPE_CHECKING

--- CIBUSmod/optimisation/geo_dist.py
+++ CIBUSmod/optimisation/geo_dist.py
@@ -9,14 +9,24 @@ import scipy
 
 import re as regex
 
-from .. import Regions, DemandAndConversions, CropProduction, FeedMgmt, ParameterRetriever
-
 from ..utils.verbose_print import verbose_init
 from ..utils.misc import multiply_aligned, inv_dict
 from ..utils.data_attr import DataAttr
 from ..main_modules.animal_herd import concat_herds
 
 from .indexed_matrix import IndexedMatrix
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .utils import Constraint
+    from .. import (
+        Regions,
+        DemandAndConversions,
+        CropProduction,
+        FeedMgmt,
+        ParameterRetriever,
+    )
 
 class GeoDistributor:
     '''Class that handles the distribution of animals and crops across regions for a given
@@ -37,7 +47,7 @@ class GeoDistributor:
     module_name = 'GeoDistributor'
 
     success: bool
-    constraints: dict[str, dict]
+    constraints: dict[str, Constraint]
     objective: dict
     problem: None | cvxpy.Problem
     x: None | dict[str, pd.Series]
