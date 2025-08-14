73f76bfcb2872e4143b2781f2c813d69e8348e77
Johan Karlsson <johan.o.karlsson@slu.se>
2025-08-14 14:59:38 +0200

    Fixed temp. response for biogenic CH4

--- CIBUSmod/impact/climate.py
+++ CIBUSmod/impact/climate.py
@@ -148,8 +148,8 @@ def get_deltaT(
 
     rename_ghg = {
         'CO2' : 'co2',
-        'CH4bio' : 'ch4',
-        'CH4fos' : 'ch4',
+        'CH4bio' : 'ch4bio',
+        'CH4fos' : 'ch4fos',
         'N2O' : 'n2o',
         'N2Oind' : 'n2o'
     }
@@ -184,6 +184,10 @@ def get_deltaT(
 
         # Pre-compute temp response curves
         temp_curves = generate_temp_responses(n=end_year+ext-start_year, model=temp_resp_model, vers=temp_resp_version)
+        # Separate fossil and biogenic CH4 by assuming that
+        # one CO2-C is sequestered per biogenic CH4-C emitted
+        temp_curves['ch4bio'] = temp_curves['ch4'] - ((12 + 2*16) / (12 + 4*1)) * temp_curves['co2'] 
+        temp_curves['ch4fos'] = temp_curves.pop('ch4')
 
         for cmp in deltaT.loc[scn].columns.unique('compound'):
             # Get GHG time-series for col
