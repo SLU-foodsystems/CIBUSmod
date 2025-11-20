b73b6ecc2122d2e07c9b60161d2aaf909b1fa92e
Johan Karlsson <johan.o.karlsson@slu.se>
2025-11-20 11:09:38 +0100

    Implemeted herd structure and feed calc. for aquaculture + some fixes in fisheries module

--- CIBUSmod/main_modules/animal_herd_fisheries.py
+++ CIBUSmod/main_modules/animal_herd_fisheries.py
@@ -6,10 +6,12 @@ from .animal_herd import AnimalHerd
 class FisheriesHerd(AnimalHerd):
     AnimalHerd.__doc__.replace('animal','wild caught fish')
 
+    has_manure = False
+
     def __init__(self,par,index,**kwargs):
 
         self.species = kwargs['species']
-        self.animals = ['fish']
+        self.animals = ['wild fish']
         self.products = ['fish']
 
         self.x_is = 'fish'
@@ -18,6 +20,30 @@ class FisheriesHerd(AnimalHerd):
 
     def calculate_herd(self):
         # No herd structure
+
+        # Create DataFrames
+        empty_df = pd.DataFrame(
+            columns = pd.MultiIndex.from_tuples([
+                (ps, ani)
+                for ps in [self.prod_system]
+                for ani in self.animals
+            ], names=['prod_system','animal']),
+            index = self.index,
+            dtype = 'float64'
+        )
+
+        # Assign values
+        heads = empty_df.copy()
+
+        # Add data attributes
+        self.data_attr.add(
+            heads, # Empty DataFrame, needed in other modules
+            name = 'heads',
+            unit = 'heads',
+            orig = 'AquacultureHerd',
+            desc = 'Total average number of heads over a year'
+        )
+
         return None
 
     def _calculate_feed_req(self):
