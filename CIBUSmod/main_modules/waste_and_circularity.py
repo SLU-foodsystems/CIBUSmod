56de620ca0b46db8db8cbcd25a442df1c41d7039
Johan Karlsson <johan.o.karlsson@slu.se>
2026-02-19 18:51:47 +0100

    Implemented method for wastewater treatment and nutrient recycling

--- CIBUSmod/main_modules/waste_and_circularity.py
+++ CIBUSmod/main_modules/waste_and_circularity.py
@@ -57,6 +57,10 @@ class WasteAndCircularity(object):
         # Define function to print progress messages if verbose==True
         vprint = verbose_init(verbose, id_str='WasteAndCircularity')
 
+        if 'wastewater_share_connected' in self.par.data.index.get_level_values('parameter'):
+            vprint('Calculating wastewater treatment ...')
+            self.calculate_wastewater()
+
         vprint('Collecting feedstocks and calculating composition ...')
         self.collect_feedstock()
 
@@ -70,7 +74,35 @@ class WasteAndCircularity(object):
 
         vprint(type='end')
 
-    def create_data_attribute_tables(self):
+    def make_data_attribute_columns(self, table):
+        # Create columns for the diferent data attributes
+
+        feedstock_cols = self.data_attr.get('feedstock_VS').columns
+
+        if 'losses' in table:
+            element = table.split('_')[1]
+            # Get compounds emitted
+            if element == 'VS':
+                cmps = ['CH4bio', 'CO2bio']
+            else:
+                cmps = self.par.get_unique('compound', qry=f'f_element == "{element}"')
+            cols = pd.MultiIndex.from_tuples(
+                [i + (cmp,) for i in feedstock_cols for cmp in cmps],
+                names = feedstock_cols.names + ['compound']
+            )
+        elif 'energy' in table:
+            level = 'energy_source' if 'use' in table else 'energy_prod'
+            ens = self.par.get_unique(level)
+            cols = pd.MultiIndex.from_tuples(
+                [i + (en,) for i in feedstock_cols for en in ens],
+                names = feedstock_cols.names + [level]
+            )
+        else:
+            cols = feedstock_cols.copy()
+
+        return cols
+
+    def create_data_attribute_tables(self, reindex=False):
         '''Creates empty dataframes and stores data attributes to write to in the
         treatment-specific methods
         '''
@@ -88,51 +120,149 @@ class WasteAndCircularity(object):
             'energy_prod' : ('kWh', 'Total energy production'),
             'energy_use' : ('kWh', 'Total energy use')
         }
-        empty_df = pd.DataFrame(
-            index=self.data_attr.get('feedstock_VS').index,
-            columns=self.data_attr.get('feedstock_VS').columns,
-        dtype = float
-        )
 
-        # Create columns for the diferent data attributes
-        for n in dfs:
-            if 'losses' in n:
-                element = n.split('_')[1]
-                # Get compounds emitted
-                if element == 'VS':
-                    cmps = ['CH4bio', 'CO2bio']
-                else:
-                    cmps = self.par.get_unique('compound', qry=f'f_element == "{element}"')
-                df = pd.DataFrame(
-                    index = empty_df.index,
-                    columns = pd.MultiIndex.from_tuples(
-                        [i + (cmp,) for i in empty_df.columns for cmp in cmps],
-                        names = empty_df.columns.names + ['compound']
-                    ),
-                    dtype = float
-                )
-            elif 'energy' in n:
-                level = 'energy_source' if 'use' in n else 'energy_prod'
-                ens = self.par.get_unique(level)
-                df = pd.DataFrame(
-                    index = empty_df.index,
-                    columns = pd.MultiIndex.from_tuples(
-                        [i + (en,) for i in empty_df.columns for en in ens],
-                        names = empty_df.columns.names + [level]
-                    ),
-                    dtype = float
-                )
-            else:
-                df = empty_df.copy()
-
-            self.data_attr.add(
-                df,
-                name = n,
-                unit = dfs[n][0],
-                orig = 'WasteAndCircularity',
-                desc = dfs[n][1]
+        # Create the diferent data attribute tables
+        for table in dfs:
+            df = pd.DataFrame(
+                index = self.data_attr.get('feedstock_VS').index,
+                columns = self.make_data_attribute_columns(table),
+                dtype = float
             )
 
+            if reindex:
+                self.data_attr.update(
+                    name = table,
+                    data = self.data_attr.get(table).reindex_like(df)
+                )
+            else:
+                self.data_attr.add(
+                    df,
+                    name = table,
+                    unit = dfs[table][0],
+                    orig = 'WasteAndCircularity',
+                    desc = dfs[table][1]
+                )
+
+    def calculate_wastewater(self):
+
+        self.par.clear()
+        p = self.par.get
+        pf = self.par.get_from_frame
+
+        #######################
+        # INCOMING WASTEWATER #
+        #######################
+
+        # Define "elements" to consider in incoming waste water
+        els = ['VS','COD','N','P','K']
+        # Get population connected to WWT
+        pop = self.demand.data_attr.get('population_per_region')
+        pop_wwt = self.demand.data_attr.get('population_per_region') * (p('wastewater_share_connected', **pop.index.to_frame().to_dict('list'))/100)
+
+        # Create dataframe for incomming VS, COD, N, P, and K
+        in_toilet = pd.DataFrame(
+            index = pop_wwt.index,
+            columns = pd.Index(els, name='element')
+        )
+        in_grey = in_toilet.copy()
+        # Calculate incoming VS, COD, N, P, and K based on parameters and population connected
+        in_toilet = pf('wastewater_incoming_toilet', in_toilet, warn_if_nan=False).mul(pop_wwt, axis=0)
+        in_grey = pf('wastewater_incoming_grey', in_toilet, warn_if_nan=False).mul(pop_wwt, axis=0)
+        # Get incoming N, P, and K based on food consumption if available
+        in_food = self.demand.data_attr.get('nutrient_supply').sum()
+        in_food = in_food.loc[list(set(in_food.index) & {'N','P','K'})].rename_axis('element')
+        in_food = pd.DataFrame(
+            np.outer(pop.transform(lambda x: x/x.sum()), in_food),
+            index = pop.index,
+            columns = in_food.index
+        )
+        # Update incoming toilet water to N, P, and K based on food consumption
+        in_toilet.update(in_food * (pop_wwt.sum()/pop.sum()))
+
+        # Make sure there are no NaNs remaining
+        assert ~in_toilet.isna().any().any(), "WasteAndCircularity: NaNs for VS, COD, N, P, or K in incoming toilet water to WWT"
+        assert ~in_grey.isna().any().any(), "WasteAndCircularity: NaNs for VS, COD, N, P, or K in incoming grey water to WWT"
+
+        # Combine toilet and grey water
+        in_total = in_toilet + in_grey
+
+        ##############
+        # ENERGY USE #
+        ##############
+
+        energy_use = pd.DataFrame(index=in_total.index, columns=in_total.columns)
+
+        #############
+        # INPUT USE #
+        #############
+
+        input_use = pd.DataFrame(index=in_total.index, columns=in_total.columns)
+
+        ##############
+        # EMISSIONS #
+        #############
+
+        # Get emitted compounds
+        cmps = self.par.get_unique('compound', qry="parameter=='wastewater_emissions'")
+        # Create dataframe for emissions
+        emissions = in_total.reindex(
+            pd.MultiIndex.from_product([in_total.columns, cmps], names=['element','compound']),
+            level = 'element',
+            axis=1
+        )
+        # Calculate emissions and sum by compound
+        emissions = (
+            (emissions * pf('wastewater_emissions', emissions))
+            .T.groupby('compound').sum().T
+        )
+
+        #####################
+        # WASTEWATER SLUDGE #
+        #####################
+
+        # Calculate VS,N,P,K in sludge
+        sludge = in_total.drop('COD', axis=1) * (pf('wastewater_recovery_sludge', in_total.drop('COD', axis=1))/100)
+
+        #########################
+        # STORE DATA ATTRIBUTES #
+        #########################
+
+        self.data_attr.add(
+            in_total,
+            name = 'wastewater.incoming',
+            unit = 'kg',
+            orig = 'WasteAndCircularity',
+            desc = 'VS, COD, N, P, and K in incoming wastewater'
+        )
+        self.data_attr.add(
+            energy_use,
+            name = 'wastewater.energy_use',
+            unit = 'kg',
+            orig = 'WasteAndCircularity',
+            desc = 'Energy use in wastewater treatment'
+        )
+        self.data_attr.add(
+            input_use,
+            name = 'wastewater.input_use',
+            unit = 'kg',
+            orig = 'WasteAndCircularity',
+            desc = 'Inputs used in wastewater treatment'
+        )
+        self.data_attr.add(
+            emissions,
+            name = 'wastewater.emissions',
+            unit = 'kg',
+            orig = 'WasteAndCircularity',
+            desc = 'Direct emissions from wastewater treatment'
+        )
+        self.data_attr.add(
+            sludge,
+            name = 'wastewater.sludge',
+            unit = 'kg',
+            orig = 'WasteAndCircularity',
+            desc = 'VS, N, P, and K in outgoing wastewater sludge'
+        )
+        return None
 
     def collect_feedstock(self):
         '''
@@ -273,9 +403,9 @@ class WasteAndCircularity(object):
             'K' : 'potassium (K)'
         }
 
-        feedstock_dfs = dict()
-        manure_dfs = dict()
+        df_dicts = []
 
+        feedstock_dfs = dict()
         for i in items:
             if i in ['B0','C']:
                 df = feedstock_dfs['VS']
@@ -285,6 +415,29 @@ class WasteAndCircularity(object):
             # Calculate item for waste
             feedstock_dfs.update({i : df.mul(self.par.get(f'feedstock_{i}'), axis=1)})
 
+        df_dicts += [feedstock_dfs]
+
+        if 'wastewater.sludge' in self.data_attr:
+            self.par.clear()
+            sludge_dfs = dict()
+            for i in items:
+                # Get item for wastewater sludge
+                if i in ['B0','C']:
+                    VS_df = sludge_dfs['VS']
+                    self.par.set(**VS_df.columns.to_frame().to_dict('list'))
+                    sludge_dfs.update({i : VS_df.mul(self.par.get(f'feedstock_{i}'), axis=1)})
+                else:
+                    s_df = self.data_attr.get('wastewater.sludge').loc[:,i].to_frame()
+                    s_df.columns = pd.MultiIndex.from_tuples(
+                        [('wastewater sludge',)*3],
+                        names=['feedstock','feedstock_group','feedstock_type']
+                        )
+                    sludge_dfs.update({i : s_df})
+
+            df_dicts += [sludge_dfs]
+        
+        manure_dfs = dict()
+        for i in items:
             # Get item from manure
             m_list = []
             for herd in (h for h in self.herds if 'manure.VS_excr' in h.data_attr):
@@ -309,7 +462,9 @@ class WasteAndCircularity(object):
                 .fillna(0)
             })
 
-        dfs = {i : pd.concat([feedstock_dfs[i], manure_dfs[i]], axis=1) for i in items}
+        df_dicts += [manure_dfs]
+
+        dfs = {i : pd.concat([df_dict[i] for df_dict in df_dicts], axis=1) for i in items}
 
         # DISTRIBUTE ACROSS WASTE TREATMENTS ----------------------------------------------------|
 
@@ -487,12 +642,17 @@ def anaerobic_digestion(waste:WasteAndCircularity):
     CH4_loss_store = CH4_loss_store_vol * CH4d
     CO2_loss_store = CO2_loss_sore_vol * CO2d
 
-    # Calculate carbon (C) in digestate after storage [kg]
-    digestate_C_to_spread = (
+    # Create dict for storing DFs of digestate composition after storage
+    digestate_after_storage = dict()
+
+    # Calculate C and VS in digestate after storage assuming that C/VS
+    # remains constant [kg]
+    digestate_after_storage['C'] = (
         digestate_C -
         CH4_loss_store * (12/(12+4*1)) -
         CO2_loss_store * (12/(12+2*16))
     )
+    digestate_after_storage['VS'] = digestate_after_storage['C'] * (feedstock_VS/feedstock_C).fillna(0)
 
     # Aggregate VS losses and update data attribute
     VS_loss = pd.concat([
@@ -500,9 +660,6 @@ def anaerobic_digestion(waste:WasteAndCircularity):
         pd.concat({'CO2bio': (CO2_loss_prod + CO2_loss_store)}, names=['compound'], axis=1).reorder_levels([1,2,3,4,0], axis=1)
     ], axis=1)
     waste.data_attr.get('losses_VS').loc[:, VS_loss.columns] = VS_loss
-
-    # Update data attribute
-    waste.data_attr.get('organic_fertiliser_C').loc[:, digestate_C_to_spread.columns] = digestate_C_to_spread
 
     # Calculate losses of NPK during digestate storage and store data attributes
     for element in ['N','P','K']:
@@ -534,16 +691,60 @@ def anaerobic_digestion(waste:WasteAndCircularity):
         else:
             loss_storage = df * 0.0
 
-        digestate_to_spread = \
+        digestate_after_storage[element] = \
             digestate - loss_storage.T.groupby(['feedstock','feedstock_group','feedstock_type','treatment']).sum().T
 
-        waste.data_attr.get('organic_fertiliser_'+element).loc[:, digestate_to_spread.columns] = digestate_to_spread
+        # Update data attribute for storage losses 
         waste.data_attr.get('losses_'+element).loc[:, loss_storage.columns] = loss_storage
 
+    # Create dicts for storing DFs of digestate to spread as fertiliser
+    # and digestate going to landfill
+    digestate_to_spread = dict()
+    digestate_to_landfill = dict()
+
+    # Get fraction of digestate assumed to be spread on
+    # cropland as organic fertiliser
+    try:
+        waste.par.clear()
+        frac_to_spread = pd.Series(
+            waste.par.get(
+                'digestate_share_as_fertiliser',
+                **digestate_after_storage['C'].columns
+                .to_frame().to_dict('list')
+            )/100,
+            index = digestate_after_storage['C'].columns
+        )
+    except KeyError:
+        # Set to 1 if parameter not defined (for compatibility)
+        frac_to_spread = 1
+    
+    # Separate digestate going to landfill and returned to cropland
+    for k,df in digestate_after_storage.items():
+        digestate_to_landfill[k] = (
+            df.mul(1-frac_to_spread, axis=1)
+            .rename(lambda x: 'digestate', axis=1)
+            .rename(lambda x: 'landfill', level='treatment', axis=1)
+            .T.groupby(df.columns.names).sum().T
+        )
+        digestate_to_spread[k] = df.mul(frac_to_spread, axis=1)
+    # Calculate B0 for digestate going to landfill
+    waste.par.clear()
+    digestate_to_landfill['B0'] = digestate_to_landfill['VS'] * gf('digestate_B0', digestate_to_landfill['VS'])
+
+    # Uppdate data attributes for digestate used as organic fertiliser
+    for element in ['C','N','P','K']:
+        waste.data_attr.get('organic_fertiliser_'+element).loc[:, digestate_to_spread[element].columns] = digestate_to_spread[element]
         if element == 'N':
             # Calculate and store plant available nitrogen in digestate
-            waste.data_attr.get('organic_fertiliser_TAN').loc[:, digestate_to_spread.columns] = \
-                digestate_to_spread * (waste.par.get_from_frame('digestate_TAN_share', digestate_to_spread)/100)
+            waste.data_attr.get('organic_fertiliser_TAN').loc[:, digestate_to_spread[element].columns] = \
+                digestate_to_spread[element] * (waste.par.get_from_frame('digestate_TAN_share', digestate_to_spread[element])/100)
+
+    # Add digestate going to landfill to the feedstock dataframes
+    # so it is handled in that treatment function
+    for element in ['C','VS','B0','N','P','K']:
+        waste.data_attr.get('feedstock_'+element)[digestate_to_landfill[element].columns] = digestate_to_landfill[element]
+        # Reindex data attribute tables with the added feedstock
+        waste.create_data_attribute_tables(reindex=True)
 
     return None
 
@@ -632,3 +833,5 @@ def landfill(waste:WasteAndCircularity):
         waste.data_attr.get(f'organic_fertiliser_{element}').loc[:,idx[:,:,:,'landfill']] = 0.0
 
     return None
+
+    
